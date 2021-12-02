05a04b80fb758ff19f92122234722c1a2812e659
Bahulyasarvala <91846471+Tutorials-cell@users.noreply.github.com>
2021-12-02 15:08:31 +0530

    mood

---
+++ MoodAnalyzer.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MoodAnalyzerUC1
+{
+    class MoodAnalyzer
+    {
+        public string str1 = "";
+        public MoodAnalyzer(string s)
+        {
+            this.str1 = s;
+        }
+        public string analyseMood()
+        {
+            try
+            {
+                string key = "Sad";
+                if (str1.Contains(key))
+                {
+                    return "SAD";
+                }
+                else
+                {
+                    return "HAPPY";
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                return e.Message;
+            }
+
+        }
+
+
+
+    }
+}
+    
---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace MoodAnalyzerUC1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter line :");
+            string line = Console.ReadLine();
+            MoodAnalyzer obj1 = new MoodAnalyzer(line);
+            Console.WriteLine(obj1.analyseMood());
+
+        }
+    }
+}
